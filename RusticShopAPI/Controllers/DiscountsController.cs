949cf6fe03daeea9e29ef56a34fa768f584e4d20
Antonioescp <packageinstaller2@gmail.com>
2023-05-08 12:04:47 -0600

    Discount Controller: Added with pagination

---
+++ RusticShopAPI/Controllers/DiscountsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RusticShopAPI.Data;
+using RusticShopAPI.Data.Models;
+using RusticShopAPI.Data.Models.DTOs;
+
+namespace RusticShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiscountsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #region CRUD
+
+        [HttpGet]
+        public async Task<ActionResult<PaginatedResult<Discount>>> GetDiscounts(
+            int pageIndex = 0,
+            int pageSize = 10,
+            string? sortColumn = null,
+            string? sortOrder = null,
+            string? filterColumn = null,
+            string? filterQuery = null)
+        {
+            return await PaginatedResult<Discount>.CreateAsync(
+                _context.Discounts,
+                pageIndex,
+                pageSize,
+                sortColumn,
+                sortOrder,
+                filterColumn,
+                filterQuery);
+        }
+
+        // GET: api/Discounts
+        [HttpGet("all")]
+        public async Task<ActionResult<IEnumerable<Discount>>> GetAllDiscounts()
+        {
+          if (_context.Discounts == null)
+          {
+              return NotFound();
+          }
+            return await _context.Discounts.ToListAsync();
+        }
+
+        // GET: api/Discounts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Discount>> GetDiscount(long id)
+        {
+          if (_context.Discounts == null)
+          {
+              return NotFound();
+          }
+            var discount = await _context.Discounts.FindAsync(id);
+
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return discount;
+        }
+
+        // PUT: api/Discounts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiscount(long id, Discount discount)
+        {
+            if (id != discount.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(discount).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiscountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Discounts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Discount>> PostDiscount(Discount discount)
+        {
+          if (_context.Discounts == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Discounts'  is null.");
+          }
+            _context.Discounts.Add(discount);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDiscount", new { id = discount.Id }, discount);
+        }
+
+        // DELETE: api/Discounts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiscount(long id)
+        {
+            if (_context.Discounts == null)
+            {
+                return NotFound();
+            }
+            var discount = await _context.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            _context.Discounts.Remove(discount);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        #endregion
+
+        #region Checks
+
+        private bool DiscountExists(long id)
+        {
+            return (_context.Discounts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        #endregion
+    }
+}
